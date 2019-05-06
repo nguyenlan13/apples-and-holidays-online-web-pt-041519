e63d44f9748de17ec3dcc157e38118565ce018b6
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-06 07:44:45 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday|
       puts "#{season.capitalize}:"
       holiday.each do |holiday, supply|
-        puts "#{holiday.capitalize}"
+        puts "#{holiday.}"
       end
     end
 end
